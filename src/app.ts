92b44d546ab14e7cdfd78fab169af1c58a3bebbb
clemersonbp <clemersonpessalli@live.com>
2021-10-22 22:02:55 -0300

    Update cors

--- src/app.ts
+++ src/app.ts
@@ -3,23 +3,24 @@ import express from "express";
 import http from "http";
 import cors from "cors";
 
-import { Server } from "socket.io"
+import { Server } from "socket.io";
 
 import { router } from "./routes";
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
 
 const io = new Server(serverHttp, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log(`Usuário conectado no socket ${socket.id}`);
-})
+});
 
 app.use(express.json());
 
